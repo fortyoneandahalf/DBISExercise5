42fb787942b6a6697425dfa24aefc9eccde77ed9
fortyoneandahalf <phil_bradfield@hotmail.com>
2016-06-07 16:22:59 +0200

    Tidied up indentation, comments, TODOs and unused members

--- src/com/dbExercise5/core/PersistenceManager.java
+++ src/com/dbExercise5/core/PersistenceManager.java
@@ -11,12 +11,8 @@ public class PersistenceManager {
     
     private static final PersistenceManager PM;
     
-    private static final String DB_FOLDER = "data";
-    private static final String LOG_FOLDER = "log";
-    
     private static Hashtable<Integer, String> pageBuffer = new Hashtable<Integer, String>();
     private static Hashtable<Integer, Transaction> transactions = new Hashtable<Integer, Transaction>();
-//    private static Hashtable<Integer, LogEntry> logBuffer;
     private static SynchronisedCounter taidGenerator;
     private static SynchronisedCounter lsnGenerator;
     
@@ -34,9 +30,9 @@ public class PersistenceManager {
     
     private PersistenceManager()
     {
-		pageBuffer = new Hashtable<Integer, String>();
-		taidGenerator = new SynchronisedCounter();
-		lsnGenerator = new SynchronisedCounter();
+	pageBuffer = new Hashtable<Integer, String>();
+	taidGenerator = new SynchronisedCounter();
+	lsnGenerator = new SynchronisedCounter();
     }
     
     public static PersistenceManager getInstance()
@@ -50,77 +46,75 @@ public class PersistenceManager {
     	Transaction ta = new Transaction(taidGenerator.value());
     	
     	lsnGenerator.increment();
-	    flushLog(lsnGenerator.value(), "BOT", ta.getTaid(), 0, "");
-	    
-		transactions.put(ta.getTaid(), ta);
-		
-		return ta.getTaid();
+    	flushLog(lsnGenerator.value(), "BOT", ta.getTaid(), 0, "");
+    	
+    	transactions.put(ta.getTaid(), ta);
+    	
+    	return ta.getTaid();
     }
     
     public void commit(int taid)
     {
-		Transaction ta = transactions.get(taid);
-		
-	    lsnGenerator.increment();
-	    flushLog(lsnGenerator.value(), "COMMIT", taid, 0, "");
-		
-		ta.setCommitted(true);
+	Transaction ta = transactions.get(taid);
+	
+	lsnGenerator.increment();
+	flushLog(lsnGenerator.value(), "COMMIT", taid, 0, "");
+	
+	ta.setCommitted(true);
     }
     
     public void write(int taid, int pageid, String data)
     {
-		pageBuffer.put(pageid, data);
-		lsnGenerator.increment();
-		flushLog(lsnGenerator.value(), "WRITE", taid, pageid, data);
-		
-		Transaction ta = transactions.get(taid);
-		ta.addPage(pageid);
-		
-		if (pageBuffer.size() > 5)
-		{
-		    flushPageBuffer();
-		}
+	pageBuffer.put(pageid, data);
+	lsnGenerator.increment();
+	flushLog(lsnGenerator.value(), "WRITE", taid, pageid, data);
+	
+	Transaction ta = transactions.get(taid);
+	ta.addPage(pageid);
+	
+	if (pageBuffer.size() > 5)
+	{
+	    flushPageBuffer();
+	}
     }
     
     private void flushLog(int lsn, String logType, int taid, int pageid, String data)
     {
-    	// TODO: write log to disk
     	LogEntry logEntry = new LogEntry(lsn, logType, taid, pageid, data);
     	FileUtilities.writeLogEntryToFile(logEntry);
     }
     
     private void flushPageBuffer()
     {
-		// Iterate through committed transactions
-		Iterator<Transaction> taIterator = transactions.values().iterator();
+	// Iterate through committed transactions
+	Iterator<Transaction> taIterator = transactions.values().iterator();
+	
+	while (taIterator.hasNext())
+	{
+	    Transaction ta = taIterator.next();
+	    
+	    if (!ta.isCommitted()) {continue;}
+	    
+	    // Iterate through pages for the committed transaction
+	    Iterator<Integer> pageIterator = ta.getPages().iterator();
+	    
+	    while (pageIterator.hasNext())
+	    {
+		// Flush the page to disk
+		int pageid = pageIterator.next();
+		flushPage(pageid);
 		
-		while (taIterator.hasNext())
-		{
-		    Transaction ta = taIterator.next();
-		    
-		    if (!ta.isCommitted()) {continue;}
-		    
-		    // Iterate through pages for the committed transaction
-		    Iterator<Integer> pageIterator = ta.getPages().iterator();
-		    
-		    while (pageIterator.hasNext())
-		    {
-				// TODO
-				// Flush the page to disk
-				// Remove the page from the buffer	
-		    	int pageid = pageIterator.next();
-		    	flushPage(pageid);
-		    	pageBuffer.remove(pageid);
-		    }
-		    
-		    // Remove the transaction from the set?
-		    transactions.remove(ta.getTaid());
-		}
+		// Remove the page from the buffer
+		pageBuffer.remove(pageid);
+	    }
+	    
+	    // Remove the transaction from the set of active transactions
+	    transactions.remove(ta.getTaid());
+	}
     }
     
     private void flushPage(int pageid)
     {
-    	// TODO: write page with given page ID to disk
     	lsnGenerator.increment();
     	Page page = new Page(pageid, lsnGenerator.value(), pageBuffer.get(pageid));
     	FileUtilities.writePageToFile(page);
