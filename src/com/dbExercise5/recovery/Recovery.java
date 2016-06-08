16487cdb76725332db90fa7b49978cd89305907b
fortyoneandahalf <phil_bradfield@hotmail.com>
2016-06-08 13:09:58 +0200

    Changed string comparisons to use .equals() and fixed indentation.

--- src/com/dbExercise5/recovery/Recovery.java
+++ src/com/dbExercise5/recovery/Recovery.java
@@ -12,49 +12,51 @@ public class Recovery {
     
     public static void recover ()
     {
-	LinkedHashSet<LogEntry> logs = new LinkedHashSet<LogEntry> ();
-	
-	// Read all logs
-	// If this were a proper system, we'd have some kind of checkpointing here
-	
-	logs = FileUtilities.readAllLogsFromFile();
-	
-	// Analyse logs and make a record of winner transactions.
-	
-	HashSet<Integer> winners = new LinkedHashSet<Integer>();
-	
-	Iterator<LogEntry> logIt = logs.iterator();
-	while (logIt.hasNext())
-	{
-	    LogEntry le = logIt.next();
-	    
-	    if (le.getLogType() == "COMMIT")
-	    {
-		winners.add(le.getTaid());
-	    }
-	}
-	
-	// Redo page modifications which were committed by winner transactions but didn't
-	// reach persistent storage
-	
-	logIt = logs.iterator();
-	while (logIt.hasNext())
-	{
-	    LogEntry le = logIt.next();
-	    
-	    Page p = FileUtilities.readPageFromFile(le.getPageid());
-	    
-	    if (le.getLsn() > p.getLsn() && winners.contains(le.getTaid()))
-	    {
-		FileUtilities.writePageToFile(new Page(le.getPageid(), le.getLsn(), le.getData()));
-	    }
-	}
+    	LinkedHashSet<LogEntry> logs = new LinkedHashSet<LogEntry> ();
+    	
+    	// Read all logs
+    	// If this were a proper system, we'd have some kind of checkpointing here
+    	
+    	logs = FileUtilities.readAllLogsFromFile();
+    	
+    	// Analyse logs and make a record of winner transactions.
+    	
+    	HashSet<Integer> winners = new LinkedHashSet<Integer>();
+    	
+    	Iterator<LogEntry> logIt = logs.iterator();
+    	while (logIt.hasNext())
+    	{
+    	    LogEntry le = logIt.next();
+    	    
+    	    if (le.getLogType().equals("COMMIT"))
+    	    {
+    	    	winners.add(le.getTaid());
+    	    }
+    	}
+    	
+    	// Redo page modifications which were committed by winner transactions but didn't
+    	// reach persistent storage
+    	
+    	logIt = logs.iterator();
+    	while (logIt.hasNext())
+    	{
+    	    LogEntry le = logIt.next();
+    	    
+    	    if (!le.getLogType().equals("WRITE")) {continue;}
+    	    
+    	    Page p = FileUtilities.readPageFromFile(le.getPageid());
+    	    
+    	    if (le.getLsn() > p.getLsn() && winners.contains(le.getTaid()))
+    	    {
+    	    	FileUtilities.writePageToFile(new Page(le.getPageid(), le.getLsn(), le.getData()));
+    	    }
+    	}
     }
 
     public static void main(String[] args) {
-	// TODO Auto-generated method stub
-	
-	recover();
+    	// TODO Auto-generated method stub
+    	
+    	recover();
 
     }
 
